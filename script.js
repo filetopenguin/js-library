17c41b8b1a9f3f66ff77b2dac3140612fee7a8f7
filetopenguin <arielweiser17@gmail.com>
2024-04-01 23:43:33 +0300

    added css

--- script.js
+++ script.js
@@ -24,12 +24,44 @@ newBookBtn.addEventListener("click",function(){
     newBookForm.style.display = "block";
 })
 
+function removeBook(index){
+myLibrary.splice(index,1);
+render();
+}
+
+Book.prototype.toggleRead= function(){
+    this.read= !this.read;
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead();
+    render();
+}
+
+
 function render(){
     let libraryEl = document.querySelector("#library");
+    libraryEl.innerHTML="";
     for(let i = 0; i<myLibrary.length; i++){
         let book = myLibrary[i];
         let bookEl = document.createElement("div");
-        bookEl.innerHTML =`<p>${book.title}</p>`;
+        bookEl.setAttribute("class","book-card");
+        bookEl.innerHTML =`
+        <div class = "card-header">
+        <h3 class ="title>${book.title}</h3>
+        <h5 class = "author"> by ${book.author}</h5>
+        </div>
+        <div class = "card-body">
+        <p> ${book.pages} pages </p>
+        <p class ="read-status">${book.read ? "Read" : "Not Read.."}</p>
+        <button class ="remove-btn" onclick="removeBook(${i})">Remove</button>
+        <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read </button>
+        </div>
+        
+        
+        `;
+
+
         libraryEl.appendChild(bookEl);
 
     }
